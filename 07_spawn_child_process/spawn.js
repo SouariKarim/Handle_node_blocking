29470c7b324554f7153bd12be58acc6062d2e265
karim <karimsouari101@gamil.com>
2022-10-04 07:52:40 +0000

    edit the spawn file

--- 07_spawn_child_process/spawn.js
+++ 07_spawn_child_process/spawn.js
@@ -17,3 +17,23 @@ bat.stderr.on('data', (data) => {
 bat.on('exit', (code) => {
   console.log(`Child exited with code ${code}`);
 });
+
+
+// =============
+
+// OR...
+const { exec, spawn } = require('node:child_process');
+exec('my.bat', (err, stdout, stderr) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(stdout);
+});
+
+// Script with spaces in the filename:
+const bat = spawn('"my script.cmd"', ['a', 'b'], { shell: true });
+// or:
+exec('"my script.cmd" a b', (err, stdout, stderr) => {
+  // ...
+});
